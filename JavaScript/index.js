4a89c3c3413bafca76fd08777e01c802cabfa322
MatiasNLopez <lopezmatias36@gmail.com>
2023-10-19 23:38:10 -0300

    Ejer3-js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,10 +3,12 @@
 /* Aca se ejecuta del ejercicio 2 al 4 */
 
 import Ejer2 from "./Ejer2.js";
+import Ejer3 from "./Ejer3.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
     const array = ["Apple", "Orange", "Banana", "Strawberry", "Grape", "Mango", "Melon","Pineapple", "Peach", "Peach"];
 
     Ejer2(array);
+    Ejer3();
     
 });
